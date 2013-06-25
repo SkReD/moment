f779a5cc9655ac49e3097bb3f613299fb38b65c5
Ivanov_Sergey <magic_89@inbox.ru>
2013-06-25 13:01:08 +0400

    Убрана минификация (с ней были проблемы).

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,9 +52,8 @@ module.exports = function (grunt) {
             options: {
                 fromString: true,
                 mangle: true,
-                compress: {
-                    dead_code: false
-                },
+	            beautify: true,
+                compress: false,
                 output: {
                     ascii_only: true
                 }
